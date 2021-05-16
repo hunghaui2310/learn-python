a86888232683938677df364a4c851c455599d60b
hunghaui2310 <hunghaui2310@gmail.com>
2021-05-16 15:15:57 +0700

    commit

---
+++ OOP/index.py
@@ -0,0 +1,13 @@
+from OOP.car import Car
+
+car_1 = Car("Chevy", "Corvette", 2021, "blue")
+car_2 = Car("Ford", "Mustang", 2022, "red")
+
+print(car_1.make)
+print(car_1.model)
+print(car_1.year)
+print(car_1.color)
+
+car_1.wheels = 2
+print(car_1.wheels)
+print(car_2.wheels)
